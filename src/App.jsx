b9cb7cb988a2ecaa19d4a24a175d2c69b1ec335e
Alderin Immanuvel S <717823s104@kce.ac.in>
2026-02-23 09:19:37 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import BmiCalculator from './Components/BmiCalculator';
+import './App.css';
+import app from './Components/App';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BmiCalculator />
+    </div>
+  );
+}
+
+export default App;
